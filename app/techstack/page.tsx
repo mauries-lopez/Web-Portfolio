a7eac50a175f9fd51aacea18200cac0fa6b8df09
Mauries Lopez <maurieslopez1800@gmail.com>
2025-12-20 14:14:37 +0800

    Progress commit
    Added:
    - Research Interest
    - TechStack View All Page

--- app/techstack/page.tsx
+++ app/techstack/page.tsx
@@ -1,7 +1,10 @@
+import TechStackPage from "@/components/page/TechStackPage";
+
 export default function Page(){
+
     return(
-        <div>
-            hello
+        <div className="h-full flex justify-center bg-white dark:bg-neutral-950 duration-500 ease-out px-5 py-10">
+            <TechStackPage/>
         </div>
     )
 }
--- components/page/TechStackPage.tsx
+++ components/page/TechStackPage.tsx
@@ -1,16 +1,22 @@
-import StackElement from "../template/StackElement";
-import { GoogleCloud, HuggingFace, Java, MongoDB, MySQL, NextJs, NPM, Python, PyTorch, React, TailwindCSS, Tensorflow, TypeScript } from "developer-icons";
+"use client"
 
-export default function TechStack(){
+import StackElement from "@/template/StackElement";
+import { C, Canva, CPlusPlus, Discord, GoogleCloud, HuggingFace, Java, Markdown, MongoDB, MySQL, NextJs, NPM, Photoshop, Python, PyTorch, React, TailwindCSS, Tensorflow, TypeScript, VisualStudioCode } from "developer-icons";
+import { useThemeToggle } from "@/hooks/useThemeToggle";
+import { Trello } from "lucide-react";
 
-    return(
-        <div className="flex flex-col items-start border border-solid border-stone-300 dark:border-neutral-700 rounded-md p-5 dark:bg-neutral-900">
+export default function TechStackPage(){
+
+    const {theme, setTheme} = useThemeToggle();
+
+    return (
+        <div className="w-250 flex flex-col items-start border border-solid border-stone-300 dark:border-neutral-700 rounded-md p-5 dark:bg-neutral-900">
+            <a href="/" className="text-xs md:text-sm flex justify-end items-center text-gray-900 dark:text-gray-100 hover:scale-101 transition cursor-pointer text-nowrap">
+                ← Back
+            </a>
             {/* Tech Stack Title */}
-            <div className="w-full flex flex-row items-center ">
+            <div className="w-full flex flex-row items-center mt-5">
                 <h1 className="w-full text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100"> Tech Stack </h1>
-                <a href="/techstack" className="text-xs md:text-sm flex justify-end items-center text-gray-900 dark:text-gray-100 hover:scale-101 transition cursor-pointer text-nowrap">
-                    View All →
-                </a>
             </div>
             {/* Tech Stacks */}
             <div className="flex flex-col gap-3 py-2">
@@ -26,7 +32,6 @@ export default function TechStack(){
                         <StackElement element={NextJs} label={"Next.js"}/>
                     </div>
                 </div>
-
                 {/* Backend */}
                 <div className="w-full flex flex-col">
                     <h2 className="font-semibold text-gray-900 dark:text-gray-100 py-1">
@@ -39,7 +44,6 @@ export default function TechStack(){
                         <StackElement element={Java} label={"Java"}/>
                     </div>
                 </div>
-
                 {/* AI & Machine Learning */}
                 <div className="w-full flex flex-col">
                     <h2 className="font-semibold text-gray-900 dark:text-gray-100 py-1">
@@ -55,7 +59,31 @@ export default function TechStack(){
                         <StackElement element={Tensorflow} label={"Tensorflow"}/>
                     </div>
                 </div>
+                {/* Other Languages */}
+                <div className="w-full flex flex-col">
+                    <h2 className="font-semibold text-gray-900 dark:text-gray-100 py-1">
+                        Other Languages
+                    </h2>
+                    <div className="flex flex-row flex-wrap gap-2">
+                        <StackElement element={C} label={"C"}/>
+                        <StackElement element={CPlusPlus} label={"C++"}/>
+                        <StackElement element={Markdown} label={"Markdown"}/>
+                    </div>
+                </div>
+                {/* Tools */}
+                <div className="w-full flex flex-col">
+                    <h2 className="font-semibold text-gray-900 dark:text-gray-100 py-1">
+                        Tools
+                    </h2>
+                    <div className="flex flex-row flex-wrap gap-2">
+                        <StackElement element={VisualStudioCode} label={"Visual Studio Code"}/>
+                        <StackElement element={Trello} label={"Trello"}/>
+                        <StackElement element={Canva} label={"Canva"}/>
+                        <StackElement element={Discord} label={"Discord"}/>
+                        <StackElement element={Photoshop} label={"Photoshop"}/>
+                    </div>
+                </div>
             </div>
-        </div>
+        </div>        
     )
 }
