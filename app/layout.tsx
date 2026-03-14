ec04a4779075d0a36d7f160da9692fa043544e2b
Mauries Lopez <maurieslopez1800@gmail.com>
2026-03-14 12:44:11 +0800

    update: page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Mauries Lopez - Software Developer",
+  title: "Mauries Lopez - Software Engineer",
   description: "",
 };
 
