f86172fc1d6d3a2b3e2f0ca500f18475d8c39e6a
Mauries Lopez <maurieslopez1800@gmail.com>
2026-03-19 22:10:06 +0800

    update: added another google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="google-site-verification" content="msrt8e_X9v3855CuI740zsVNgXL7cx4lAWoESaK7kC4" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-neutral-950 duration-500 ease-out`}>
         {children}
