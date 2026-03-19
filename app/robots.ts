f9b1110cd3c6802e5d5de4764da4de124c723e92
Mauries Lopez <maurieslopez1800@gmail.com>
2026-03-19 22:43:36 +0800

    update: nextjs sitemap

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://maurieslopez.netlify.app/sitemap.xml',
+  }
+}
